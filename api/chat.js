7811f2546ba989ee43abcd04fa356abaf4d1b583
abdalhajzein-art <abd.alhajzein@gmail.com>
2026-07-19 02:39:30 +0300

    Change model from llama-3.1-70b to llama-3.1-8b

--- api/chat.js
+++ api/chat.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
         "Authorization": `Bearer ${apiKey}`
       },
       body: JSON.stringify({
-        model: "llama-3.1-70b-instant",
+        model: "llama-3.1-8b-instant",
         messages: [
           {
             role: "user",
